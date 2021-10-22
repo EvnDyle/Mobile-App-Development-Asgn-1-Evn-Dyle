9f8d6a4dcd4bc2c680f4231b495d3cdcc782a26d
EvnDyle <evandoyle77@gmail.com>
2021-10-22 12:49:17 -0400

    Create, Read, Update, Search added, JSON persistence added, additional refactoring added

--- src/main/kotlin/assignment/main.kt
+++ src/main/kotlin/assignment/main.kt
@@ -1,61 +1,9 @@
 package assignment
 
-import mu.KotlinLogging
-import models.Character
+import CharacterController
 
-private val logger = KotlinLogging.logger {}
-var character = Character()
+val controller = CharacterController()
 
-fun main(args: Array<String>){
-    logger.info { "Launching Evan's Character Manager Console App" }
-
-    var input: Int
-
-    do {
-        input = menu()
-        when(input) {
-            1 -> addCharacter()
-            2 -> println("You Chose Update Character")
-            3 -> println("You Chose List All Characters")
-            4 -> println("You Delete a Character")
-            -1 -> println("Exiting App")
-            else -> println("Invalid Option")
-        }
-        println()
-    } while (input != -1)
-    logger.info { "Shutting Evan's Character Manager Console App" }
-
-}
-
-fun menu() : Int {
-
-    var option : Int
-    var input: String? = null
-
-    println("Main Menu")
-    println(" 1. Add Character")
-    println(" 2. Update Character")
-    println(" 3. List All Characters")
-    println(" 4. Delete a Character")
-    println("-1. Exit")
-    println()
-    print("Enter an integer : ")
-    input = readLine()!!
-    option = if (input.toIntOrNull() != null && !input.isEmpty())
-        input.toInt()
-    else
-        -9
-    return option
-}
-
-fun addCharacter(){
-    println("Add Character\n")
-    print("Enter a Name : ")
-    character.name = readLine()!!
-    print("Enter a Race : ")
-    character.race = readLine()!!
-    print("Enter a Class : ")
-    character.clss = readLine()!!
-    println("You entered [ " + character.name + " ] for name " +
-            "and [ " + character.race + " ] for race and [ "+ character.clss + " ] for class")
+fun main(args: Array<String>) {
+    controller.start()
 }
---
+++ src/main/kotlin/models/CharacterModel.kt
@@ -0,0 +1,10 @@
+package models
+
+class CharacterModel (
+
+    var id: Long = 0,
+    var name: String = "",
+    var race: String = "",
+    var clss: String = "",
+)
+
